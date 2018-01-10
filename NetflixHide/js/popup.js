3291c2bcfa0b262e1b7224c7defe5575fc248ca8
Rico <jun.he.833@gmail.com>
2018-01-09 21:41:25 -0500

    Rename NetflixHide/popup.js to NetflixHide/js/popup.js

---
+++ NetflixHide/js/popup.js
@@ -0,0 +1,1 @@
+chrome.contextMenus.create({"title":"Hide/Tint this title","id":"Remove","contexts":["all"],"documentUrlPatterns":["*://*.netflix.com/*"]});chrome.contextMenus.onClicked.addListener(function(info,tab){chrome.tabs.query({active:true,currentWindow:true},function(tabs){chrome.tabs.sendMessage(tabs[0].id,info.menuItemId);});});
